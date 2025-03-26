efa07e2fe3e97a64634a54c90683588b567ee7da
mjl <2404816298@qq.com>
2025-03-26 17:21:29 +0800

    安装prop-type类型校验

--- src/renderer/src/components/Button/Button.jsx
+++ src/renderer/src/components/Button/Button.jsx
@@ -1,4 +1,10 @@
 import './Button.scss'
+import PropTypes from 'prop-types'
+
+Button.propTypes = {
+  content: PropTypes.string,
+  onClick: PropTypes.func
+}
 export default function Button({ content, onClick }) {
   return (
     <button className="btn" onClick={onClick}>
